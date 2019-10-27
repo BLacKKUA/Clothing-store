74e8fd92e0fbe9e88aca9cdac4ef1344468720e2
BLacKKUA <rvalko.dev@gmail.com>
2019-10-27 18:22:37 +0200

    creating pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminCategoryComponent } from './admin/admin-category/admin-category.component';
+import { AdminProductComponent } from './admin/admin-product/admin-product.component';
+import { AdminUserComponent } from './admin/admin-user/admin-user.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    AdminCategoryComponent,
+    AdminProductComponent,
+    AdminUserComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    ShopComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
